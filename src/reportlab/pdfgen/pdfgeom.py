0f3c0a47f0b42f6619cd7d7af11603003d141de0
Dow Drake <dowdrake@gmail.com>
2023-07-25 20:29:59 -0700

    Format to PEP8 standard.

--- src/reportlab/pdfgen/pdfgeom.py
+++ src/reportlab/pdfgen/pdfgeom.py
@@ -1,8 +1,9 @@
-#Copyright ReportLab Europe Ltd. 2000-2017
-#see license.txt for license details
-#history https://hg.reportlab.com/hg-public/reportlab/log/tip/src/reportlab/pdfgen/pdfgeom.py
-__version__='3.3.0'
-__doc__="""
+# Copyright ReportLab Europe Ltd. 2000-2017
+# see license.txt for license details
+# history https://hg.reportlab.com/hg-public/reportlab/log/tip/src
+#                                  /reportlab/pdfgen/pdfgeom.py
+__version__ = '3.3.0'
+"""
 This module includes any mathematical methods needed for PIDDLE.
 It should have no dependencies beyond the Python library.
 
@@ -12,7 +13,7 @@ So far, just Robert Kern's bezierArc.
 from math import sin, cos, pi, ceil
 
 
-def bezierArc(x1,y1, x2,y2, startAng=0, extent=90):
+def bezierArc(x1, y1, x2, y2, startAng=0, extent=90):
     """bezierArc(x1,y1, x2,y2, startAng=0, extent=90) --> List of Bezier
 curve control points.
 
@@ -27,14 +28,14 @@ The resulting coordinates are of the form (x1,y1, x2,y2, x3,y3, x4,y4)
 such that the curve goes from (x1, y1) to (x4, y4) with (x2, y2) and
 (x3, y3) as their respective Bezier control points."""
 
-    x1,y1, x2,y2 = min(x1,x2), max(y1,y2), max(x1,x2), min(y1,y2)
+    x1, y1, x2, y2 = min(x1, x2), max(y1, y2), max(x1, x2), min(y1, y2)
 
     if abs(extent) <= 90:
-        arcList = [startAng]
+        # arcList = [startAng] (assigned but not used)
         fragAngle = float(extent)
         Nfrag = 1
     else:
-        arcList = []
+        # arcList = [] (assigned but not used)
         Nfrag = int(ceil(abs(extent)/90.))
         fragAngle = float(extent) / Nfrag
 
@@ -45,16 +46,17 @@ such that the curve goes from (x1, y1) to (x4, y4) with (x2, y2) and
     halfAng = fragAngle * pi / 360.
     kappa = abs(4. / 3. * (1. - cos(halfAng)) / sin(halfAng))
 
-    if fragAngle < 0:
-        sign = -1
-    else:
-        sign = 1
+    # sign assigned but not used
+    # if fragAngle < 0:
+    #     sign = -1
+    # else:
+    #     sign = 1
 
     pointList = []
 
     for i in range(Nfrag):
         theta0 = (startAng + i*fragAngle) * pi / 180.
-        theta1 = (startAng + (i+1)*fragAngle) *pi / 180.
+        theta1 = (startAng + (i+1)*fragAngle) * pi / 180.
         if fragAngle > 0:
             pointList.append((x_cen + rx * cos(theta0),
                               y_cen - ry * sin(theta0),
