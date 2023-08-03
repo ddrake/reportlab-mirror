723eca61e0c3f604f42d1c962afe084d766f92ff
Dow Drake <dowdrake@gmail.com>
2023-08-03 11:41:01 -0700

    Remove unused (and broken) function

--- src/reportlab/lib/attrmap.py
+++ src/reportlab/lib/attrmap.py
@@ -164,16 +164,17 @@ def _findObjectAndAttr(src, P):
         return src, P[-1]
 
 
-def hook__setattr__(obj):
-    if not hasattr(obj, '__attrproxy__'):
-        C = obj.__class__
-        import new
-        obj.__class__ = new.classobj(
-            C.__name__, (C, )+C.__bases__,
-            {'__attrproxy__': [],
-             '__setattr__': (lambda self, k, v, osa=getattr(obj, '__setattr__', None),
-                             # bug? hook is not defined!
-                             hook=hook: hook(self, k, v, osa))})  # noqa
+# Not used anywhere
+# def hook__setattr__(obj):
+#     if not hasattr(obj, '__attrproxy__'):
+#         C = obj.__class__
+#         import new
+#         obj.__class__ = new.classobj(
+#             C.__name__, (C, )+C.__bases__,
+#             {'__attrproxy__': [],
+#              '__setattr__': (lambda self, k, v, osa=getattr(obj, '__setattr__', None),
+#                              # bug? hook is not defined!
+#                              hook=hook: hook(self, k, v, osa))})  # noqa
 
 
 def addProxyAttribute(src, name, validate=None, desc=None, initial=None, dst=None):
